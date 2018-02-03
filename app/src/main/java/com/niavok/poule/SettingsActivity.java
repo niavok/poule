dadc3a0fdd6afd21e9790338ad11be24e473974d
Frédéric Bertolus <fred.bertolus@gmail.com>
2018-02-03 23:05:53 +0100

    Implements login system

--- app/src/main/java/com/niavok/poule/SettingsActivity.java
+++ app/src/main/java/com/niavok/poule/SettingsActivity.java
@@ -37,7 +37,7 @@ public class SettingsActivity extends AppCompatActivity {
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "plop:hello", "bar@example.com:world"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -49,7 +49,8 @@ public class SettingsActivity extends AppCompatActivity {
     private EditText mPasswordView;
     private EditText mSecurityView;
     private View mProgressView;
-    private View mLoginFormView;
+    private View mLoginSuccessView;
+    private View mLoginFailView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +69,8 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
+        mLoginSuccessView = findViewById(R.id.login_success);
+        mLoginFailView = findViewById(R.id.login_fail);
         mProgressView = findViewById(R.id.login_progress);
     }
 
@@ -125,13 +127,14 @@ public class SettingsActivity extends AppCompatActivity {
         // Store values at the time of the login attempt.
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
+        String security = mSecurityView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
         }
@@ -141,9 +144,17 @@ public class SettingsActivity extends AppCompatActivity {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
             cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+        }
+
+        // Check for a valid security work
+        if (TextUtils.isEmpty(security)) {
+            mSecurityView.setError(getString(R.string.error_field_required));
+            focusView = mSecurityView;
+            cancel = true;
+        }
+        else if(!security.equalsIgnoreCase("loki")) {
+            mSecurityView.setError("This is not the best cat! Shame!");
+            focusView = mSecurityView;
             cancel = true;
         }
 
@@ -160,50 +171,21 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
-
     /**
      * Shows the progress UI and hides the login form.
      */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        if(show)
+        {
+            mLoginSuccessView.setVisibility(View.GONE);
+            mLoginFailView.setVisibility(View.GONE);
         }
+    }
+
+    private void loginStatus(final boolean success) {
+        mLoginSuccessView.setVisibility(success ? View.VISIBLE : View.GONE);
+        mLoginFailView.setVisibility(success ? View.GONE : View.VISIBLE);
     }
 
     private interface ProfileQuery {
@@ -232,25 +214,10 @@ public class SettingsActivity extends AppCompatActivity {
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+            GSLogin loginQuery = new GSLogin(mEmail, mPassword);
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
+            loginQuery.Query();
+            return loginQuery.IsLogged();
         }
 
         @Override
@@ -259,8 +226,9 @@ public class SettingsActivity extends AppCompatActivity {
             showProgress(false);
 
             if (success) {
-                finish();
+                loginStatus(true);
             } else {
+                loginStatus(false);
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
             }
