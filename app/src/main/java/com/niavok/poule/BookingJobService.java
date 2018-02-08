0ac0286ea78328e6f2964ce006bd3d3b1ad12308
Frédéric Bertolus <fred.bertolus@gmail.com>
2018-02-08 22:20:28 +0100

    Implement notifications

--- app/src/main/java/com/niavok/poule/BookingJobService.java
+++ app/src/main/java/com/niavok/poule/BookingJobService.java
@@ -2,11 +2,9 @@ package com.niavok.poule;
 
 import android.app.IntentService;
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 
@@ -26,6 +24,7 @@ public class BookingJobService extends IntentService {
     {
         public String failCause;
         public PouleConfig config;
+        public String activity;
     }
 
     @Override
@@ -37,6 +36,7 @@ public class BookingJobService extends IntentService {
     private void onHandleIntentSafe(Intent intent) {
         BookingContext bookingContext = new BookingContext();
         bookingContext.config = new PouleConfig(getApplicationContext());
+        bookingContext.activity = "Unknown";
         bookingContext.failCause = "Unknown cause";
 
         int day  = intent.getIntExtra("day", -1);
@@ -50,23 +50,25 @@ public class BookingJobService extends IntentService {
         if(day == -1)
         {
             bookingContext.failCause = "Invalid booking day";
-            notifyResaFailed();
+            notifyResaFailed(bookingContext);
             return;
         }
 
         if(locationId == null || locationName == null)
         {
             bookingContext.failCause = "Invalid booking location";
-            notifyResaFailed();
+            notifyResaFailed(bookingContext);
             return;
         }
 
         if(activityLevel == null || activityLocation == null || activityTime == null)
         {
             bookingContext.failCause = "Invalid booking activity";
-            notifyResaFailed();
+            notifyResaFailed(bookingContext);
             return;
         }
+
+        bookingContext.activity = activityTime;
 
         GsLocation resaLocation = new GsLocation(locationId, locationName);
         GsDay resaDay = new GsDay(day);
@@ -78,7 +80,7 @@ public class BookingJobService extends IntentService {
 
             if(makeReservation(bookingContext, resaLocation, resaDay, resaActivity))
             {
-                notifyResaSuccess();
+                notifyResaSuccess(bookingContext);
                 return;
             }
 
@@ -93,40 +95,39 @@ public class BookingJobService extends IntentService {
             }
         }
 
-        notifyResaFailed();
+        notifyResaFailed(bookingContext);
     }
 
-    private void notifyResaFailed() {
+    private void notifyResaFailed(BookingContext bookingContext) {
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         NotificationCompat.Builder noti = new NotificationCompat.Builder(getApplicationContext());
 
-                noti.setContentTitle("Booking failed")
-                .setContentText("plop")
+                noti.setContentTitle("Booking failed: "+ bookingContext.activity)
+                .setContentText(bookingContext.failCause)
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setPriority(Notification.PRIORITY_MAX)
-        ///mBuilder.setStyle(bigText);
-                //.setLargeIcon(R.drawable.ic_menu_gallery)
-           //     .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                .setLights(0x4e0099, 100, 100)
+                .setVibrate(new long[]{100, 200, 100, 200, 100, 400, 300, 400, 300, 400, 300, 100, 200, 100, 200, 100, 400, 300, 400, 300, 400, 300})
                 ;
 
-        mNotificationManager.notify(1, noti.build());
+        mNotificationManager.notify(bookingContext.activity.hashCode(), noti.build());
     }
 
-    private void notifyResaSuccess() {
+    private void notifyResaSuccess(BookingContext bookingContext) {
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        Notification noti = new Notification.Builder(getApplicationContext())
-                .setContentTitle("New mail from ")
-                .setContentText("plop")
-                .setSmallIcon(R.drawable.ic_menu_manage)
-                .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
-                //.setLargeIcon(aBitmap)
-                .build();
+        NotificationCompat.Builder noti = new NotificationCompat.Builder(getApplicationContext());
 
-        mNotificationManager.notify(0, noti);
+        noti.setContentTitle("Booking success: "+ bookingContext.activity)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setPriority(Notification.PRIORITY_LOW)
+                .setLights(0x4e0099, 100, 5000)
+        ;
+
+        mNotificationManager.notify(bookingContext.activity.hashCode(), noti.build());
     }
 
     private boolean makeReservation(BookingContext bookingContext, GsLocation resaLocation, GsDay resaDay, GsActivity resaActivity) {
